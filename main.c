35f0ac2ea59e3cbfac3eef7c95f4a52cbc08be77
sebseb7 <github@seb.exse.net>
2016-08-08 01:27:00 +0200

    doc&fix

--- main.c
+++ main.c
@@ -13,9 +13,25 @@ int main(int argc __attribute__((__unused__)), char *argv[] __attribute__((__unu
 
 	int zoom = 21;
 
+	/*
+	 * save example:
+	
+	char *file_name = "save";
+	char *base_path = SDL_GetPrefPath("net.exse", "sdlboudlerdash");
+	char *file_path = calloc((strlen(base_path)+strlen(file_name)),sizeof(char));
+	strcat(file_path,base_path);
+	strcat(file_path,file_name);
+	free(base_path);	
+	SDL_RWops *file = SDL_RWFromFile(file_path, "w");
+	free(file_path);
+	SDL_RWclose(file);
+
+	*/
+
+
 	unsigned int* pixelbuffer = sdl_init(CAVE_WIDTH*zoom, (INFO_HEIGHT+CAVE_HEIGHT)*zoom,"Boulderdash",60);
 
-	struct bd_game_struct_t* bd_game = bd_game_initialize(8,0);
+	struct bd_game_struct_t* bd_game = bd_game_initialize(0,0);
 
 	int limiter=0;
 
